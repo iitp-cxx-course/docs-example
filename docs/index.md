fa113d9ed6619e9e20bd55a0b783a6bcd93d9de0
Alexey Kreshchuk <akrsch@gmail.com>
2021-04-29 15:14:19 +0300

    add docs

---
+++ docs/index.md
@@ -0,0 +1,5 @@
+# Example project {#index}
+
+The main class of this project is S
+
+We also have `not_random`
--- src/lib.hpp
+++ src/lib.hpp
@@ -1,3 +1,25 @@
 #pragma once
 
+/// Return non-random number
 extern int not_random();
+
+/// Some struct
+/**
+ * Does something unknown
+ */
+struct S {
+    int a; ///< int field
+
+    /// float field
+    float b;
+
+    /**
+     * \brief Set all
+     *
+     * Sets all fields
+     * \param a int field
+     * \param b float field
+     * \return nothing
+     */
+    void set(int a, int b);
+};
